8796db86def92ead1ee17a5663b1a36973c81bfb
Kim <331599158@qq.com>
2020-12-15 13:03:50 +0800

    增加题卡

--- exam/src/components/directory.jsx
+++ exam/src/components/directory.jsx
@@ -1,14 +1,9 @@
-import React, { PureComponent } from 'react';
-import { Button } from 'antd';
+import React from 'react';
+import { Pagination } from 'antd';
 
-export default class Directory extends PureComponent {
-  render() {
-    return (
-      <>
-        <Button shape="circle">1</Button>
-        <Button shape="circle">2</Button>
-        <Button shape="circle">3</Button>
-      </>
-    );
-  }
-}
+export default () => (
+  <Pagination
+    defaultCurrent={2}
+    total={50}
+  />
+);
