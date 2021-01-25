72522bb042847c00ff73cbd89ae8e4d5d0fca198
Kim <331599158@qq.com>
2021-01-25 09:28:06 +0800

    调整样式

--- exam/src/components/Audio.jsx
+++ exam/src/components/Audio.jsx
@@ -2,22 +2,35 @@
  * 音频播放组件
  */
 import React from 'react';
+import { Card, Typography, Row, Col } from 'antd';
+
+const { Title, Text } = Typography;
 
 export default ({ q }) => {
   if (!q.ext || !q.ext.audio) {
     return '';
   }
 
-  return q.ext.audio.map(a => (
-    <div>
-      {a.name}
-      <audio
-        key={`${q.id}_${a.name}`}
-        src={a.src}
-        controls="controls"
-      >
-        Your browser does not support the audio element.
-      </audio>
-    </div>
+  const arr = q.ext.audio.map(a => (
+    <Row key={`${q.id}_${a.name}`} align="middle">
+      <Col md={{ span: 2 }}>
+        <Text>{a.name}</Text>
+      </Col>
+      <Col md={{ span: 22 }}>
+        <audio
+          src={a.src}
+          controls="controls"
+        >
+          Your browser does not support the audio element.
+        </audio>
+      </Col>
+    </Row>
   ));
+
+  return (
+    <Card style={{ marginTop: 8 }}>
+      <Title level={5}>音频资料</Title>
+      {arr}
+    </Card>
+  );
 };
