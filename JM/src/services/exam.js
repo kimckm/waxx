0d8ca54c9235b1b3410809103ead70dea72c682c
Kim <331599158@qq.com>
2021-10-14 09:24:38 +0800

    清除多余代码

--- JM/src/services/exam.js
+++
@@ -1,5 +0,0 @@
-export async function list(params) {
-  return fetch(`/api/exams`)
-    .then(res => res.json())
-    .catch(console.error);
-}
--- JM/src/services/practice.js
+++
@@ -1,8 +0,0 @@
-import qs from 'querystring';
-
-// 随机抽题
-export async function random(params) {
-  return fetch(`/api/practice/random?${qs.stringify(params)}`)
-    .then(res => res.json())
-    .catch(console.error);
-}
