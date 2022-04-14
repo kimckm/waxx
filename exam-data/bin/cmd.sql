b7938212c00fc422d96be88870622a822e52d3f6
Kim <331599158@qq.com>
2022-04-14 11:54:33 +0800

    f

--- exam-data/bin/cmd.sql
+++
@@ -1,2 +0,0 @@
-/* 增加外键约束 */
-alter table table_name add contraint fk_name foreign key fk_id references fk_table(id);
--- exam-data/bin/dump.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATE=$(date "+%Y-%m-%d")
-
-docker run -it --rm mariadb mysqldump \
-  -h8.135.66.238 \
-  -uroot \
-  -pAbcd1234 exam > exam-${DATE}.sql
